68a725b0878982df809aaac9e252f5981f8bee9e
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-06 19:21:00 +0300

    feat: setup updated

--- README.md
+++ README.md
@@ -21,11 +21,13 @@ zsh -c "$(curl -fsSL https://raw.githubusercontent.com/yazilim-vip/maescript/mai
 
 ## Install manually
 1. Clone repository into a directory
-2. For bash; add following line to your .bashrc file
+2. For bash; add following lines to your .bashrc file
    ```sh
+   MAESCRIPT_HOME=/path/to/repo
    source /path/to/repo/init.sh bash
    ``` 
 3. For zsh; add following line to your .zshrc file
    ```sh
+   MAESCRIPT_HOME=/path/to/repo
    source /path/to/repo/init.sh zsh
    ```
--- init.sh
+++ init.sh
@@ -1,22 +1,5 @@
 # Locate Script DIR
 shell_mode=$1
-case $shell_mode in
-    zsh)
-        x=${(%):-%N}
-        SCRIPT_DIR=${x:A:h}
-        ;;
-
-    bash)
-	    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-        ;;
-    *)
-        echo "Failed to detect shell_mode"
-        exit 1
-        ;;
-esac
-
-# Installation Directory of Maescript
-MAESCRIPT_HOME=$SCRIPT_DIR
 
 # Current Version of Maescript
 MAESCRIPT_VERSION="v1.00-beta"
@@ -33,6 +16,7 @@ MAESCRIPT_USER_BACUP_DIR=$MAESCRIPT_USER_CONFIG_DIR
 # Initialize Binary Path
 PATH=$PATH:$MAESCRIPT_HOME/bin
 
+echo "here"
 # Aliases
 alias reconf='source ~/.bashrc'
 
